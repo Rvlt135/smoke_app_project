feead1c404bbcafac75685ceaa2476b30bdb222c
rvlt135 <rvlt135@gmail.com>
2023-02-08 17:24:30 +0100

    add docker-compose + postgres

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # smoke_app_project
-
-1. pip install -r requirements.txt
+1. cd django_smoke_app
+2. docker-compose up
+3. pip install -r requirements.txt
+4. python3 manage.py migrate
--- django_smoke_app/django_smoke_app/settings.py
+++ django_smoke_app/django_smoke_app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'smoke_app',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'django_smoke_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test_django_db',
+        'USER': 'user',
+        'PASSWORD': '123qwe',
+        'HOST': '127.0.0.1',
+        'PORT': '5433',
     }
 }
 
